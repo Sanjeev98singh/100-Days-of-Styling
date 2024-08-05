24df71fa8bccce3614473e109e9d5bac577f9e63
Sanjeev <coolviccy123@gmail.com>
2024-08-05 17:08:32 +0530

    updated

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'custom': '0 0 20px 0 rgba(0, 0, 0, 0.3)',
+      },
+      borderWidth: {
+        '44': '44px',
+      },
+      fontFamily: {
+        'courier-prime': ['"Courier Prime"', 'monospace'],
+      },
+      fontWeight: {
+        'regular': '400',
+        'bold': '700',
+      }
+    },
+  },
+  plugins: [],
+}
+
